9504d573cb290b40028347e6f8ccb344d6490974
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-11 23:10:22 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+// $ npm install react-icons --save
+// $ npm install --save normalize.css
+
+
+// 1
+// Додамо до проекту бібліотеку Redux - набір функцій для створення стора (сховища, store),
+// роботи зі станом програми (state) та відправлення екшенів (дій, actions).
+
+// $ npm install redux
+
+// Для використання React та Redux разом, необхідно додати до проекту бібліотеку React Redux.
+// Це набір компонентів та хуків зв'язуючих компонентів React та Redux стор (store).
+
+// $ npm install react-redux
+
+
 import ReactDOM from "react-dom/client";
 import { App } from "components/App";
 import "./index.css";
