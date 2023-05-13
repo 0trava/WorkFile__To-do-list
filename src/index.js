b5e79917e3c9dee460a9852eabc400a4ee1ee398
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-13 20:24:14 +0300

    ad  story redux

--- src/index.js
+++ src/index.js
@@ -13,10 +13,29 @@
 
 // $ npm install react-redux
 
+// 2
+
+// Після створення стору необхідно зв'язати його з компонентами React, 
+// щоб вони могли отримувати доступ до стору та його методів. Для цього у 
+// бібліотеці React Redux є компонент Provider, котрий чекає однойменний пропс store. 
+// Для того щоб будь-який компонент у додатку міг використовувати стор, обертаємо 
+// Provider все дерево компонентів.
+
 
 import ReactDOM from "react-dom/client";
 import { App } from "components/App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// 2 Додали
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
+// БУЛО
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// 2 Додали
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
