dcaf59e7cb7df25d01c21b61787cf57071deb0cf
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-13 21:07:07 +0300

    add

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,7 +4,7 @@ import css from "./Task.module.css";
 // Імпортуємо хук
 import { useDispatch } from "react-redux";
 // Імпортуємо генератор екшену
-import { deleteTask } from "../../redux/actions";
+import { deleteTask, toggleCompleted } from "../../redux/actions";
 
 
 export const Task = ({ task }) => {
@@ -17,12 +17,17 @@ export const Task = ({ task }) => {
       // Відправляємо результат - екшен видалення завдання
       const handleDelete = () => dispatch(deleteTask(task.id));
 
+       // Викликаємо генератор екшену та передаємо ідентифікатор завдання
+      // Відправляємо результат - екшен перемикання статусу завдання
+      const handleToggle = () => dispatch(toggleCompleted(task.id));
+
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
         checked={task.completed}
+        onChange={handleToggle}
       />
       <p className={css.text}>{task.text}</p>
       <button className={css.btn} onClick={handleDelete}>
